5a7319ab60dfef75a748472dd520bd270191918a
Vrganj <luka.barbic@hotmail.com>
2019-04-17 22:11:55 +0200

    harambe

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import discord, json, sys, random
+from random import choice, randint
 from discord.ext import commands
 from cards import generate_cards
+import discord, json, sys
 
 bot = commands.Bot(command_prefix='!')
 
 colors     = ["r", "y", "g", "b"]
 cards      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "skip", "reverse", "d2"]
-playerDeck = {
-    '53626939': ['r_5', 'g_6', 'y_reverse', 'b_skip', 'r_skip', 'y_2', 'g_d2']
-}
+queue      = []
+games      = {}
+playerDeck = {}
 
 with open('config.json') as file:
     config = json.load(file)
@@ -34,5 +35,20 @@ async def deck(ctx):
     else:
         generate_cards(playerDeck.get(str(ctx.message.author.id)[:8]))
         await ctx.send('Here\'s your deck!', file=discord.File('last.png', 'deck.png'))
+
+@bot.command()
+async def play(ctx):
+    if queue.__contains__(ctx.message.author.id):
+        queue.remove(ctx.message.author.id)
+        await ctx.send(f'Left the queue. **{len(queue)}** in queue now.')
+    elif playerDeck.get(str(ctx.message.author.id)[:8]):
+        await ctx.send('You\'re currently in a game!')
+    elif not queue.__contains__(ctx.message.author.id):
+        queue.append(ctx.message.author.id)
+        await ctx.send(f'Joined the queue! **{len(queue)}** in the queue now.')
+        if len(queue) == 2:
+            ctx.send('The game should start in about 15 seconds!')
+    else:
+        ctx.send('Something strange is happening! :ghost:')
 
 bot.run(config['token'])
