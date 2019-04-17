5e5eb9e8b1854953dbaa0066f33f993c9110f9b7
Vrganj <luka.barbic@hotmail.com>
2019-04-17 17:08:33 +0200

    Shit

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ colors = ["r", "y", "g", "b"]
 
 cards = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "skip", "reverse", "d2"]
 
+playerDeck = {
+    '536269399783505950': ['r_5', 'y_reverse', 'g_9', 'g_d2', 'b_3', 'y_7', 'b_2']
+}
+
 bot = commands.Bot(command_prefix='!')
 
 with open('config.json') as file:
@@ -17,23 +21,33 @@ async def randdeck(ctx):
         images = []
         for id in range(7):
             id = random.choice(colors) + '_' + random.choice(cards)
-            images.append(f'cards/{id}.png')
-        img = map(Image.open, images)
-
-        image = Image.new('RGBA', (242 * len(images), 362), (255, 0, 0, 0))
-
-        x_offset = 0
-        for im in img:
-            image.paste(im, (x_offset, 0))
-            x_offset += im.size[0]
-
-        image.save('join.png', 'PNG')
+            images.append(id)
+        genCards(images)
         await ctx.send('', file=discord.File('join.png', 'deck.png'))
     except:
         await ctx.send('Looks like you entered an invalid card ID.')
 
 @bot.command()
 async def deck(ctx):
-    print(ctx.message.author)
+    if not playerDeck.get(str(ctx.message.author.id)):
+        await ctx.send('You aren\'t in a game!')
+    else:
+        genCards(playerDeck.get(str(ctx.message.author.id)))
+        await ctx.send('Here\'s your deck!', file=discord.File('join.png', 'deck.png'))
+
+def genCards(images):
+    for i in range(len(images)):
+        images[i] = 'cards/' + images[i] + '.png'
+
+    img = map(Image.open, images)
+
+    image = Image.new('RGBA', (242 * len(images), 362), (255, 0, 0, 0))
+
+    x_offset = 0
+    for im in img:
+        image.paste(im, (x_offset, 0))
+        x_offset += im.size[0]
+
+    image.save('join.png', 'PNG')
 
 bot.run(config['token'])
