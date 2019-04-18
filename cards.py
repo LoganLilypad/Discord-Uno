b7450f2ee68873f46c4e663829344db181b08b4d
Vrganj <luka.barbic@hotmail.com>
2019-04-18 15:34:51 +0200

    Added the wild cards

--- cards.py
+++ cards.py
@@ -4,12 +4,10 @@ def generate_cards(images):
     for i in range(len(images)):
         images[i] = 'cards/' + images[i] + '.png'
 
-    img = map(Image.open, images)
-
     image = Image.new('RGBA', (242 * len(images), 362), (255, 0, 0, 0))
 
     x_offset = 0
-    for im in img:
+    for im in map(Image.open, images):
         image.paste(im, (x_offset, 0))
         x_offset += im.size[0]
 
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import discord, json, sys
 
 bot = commands.Bot(command_prefix='!')
 
-colors     = ["r", "y", "g", "b"]
-cards      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "skip", "reverse", "d2"]
-queue      = []
-games      = {}
-playerDeck = {}
+games, playerDeck = {}
+colors = ["r", "y", "g", "b"]
+cards  = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "skip", "reverse", "d2"]
+queue  = []
+
 
 with open('config.json') as file:
     config = json.load(file)
@@ -19,8 +19,7 @@ async def randdeck(ctx):
     try:
         images = []
         for id in range(7):
-            id = random.choice(colors) + '_' + random.choice(cards)
-            images.append(id)
+            images.append(random.choice(colors) + '_' + random.choice(cards))
         generate_cards(images)
         await ctx.send('', file=discord.File('last.png', 'deck.png'))
     except:
@@ -31,7 +30,7 @@ async def deck(ctx):
     if not playerDeck.get(str(ctx.message.author.id)[:8]):
         await ctx.send('You aren\'t in a game!')
     elif ctx.message.channel.name[-8:] != str(ctx.message.author.id)[:8]:
-        await ctx.send(f'You can only use this command in your private channel! The channel is in UNO category and should look something like #{ctx.message.author.name.lower()}-{str(ctx.message.author.id)[:8]}.')
+        await ctx.send(f'You can only use this command in your private channel! The channel is in UNO category and should look like #{ctx.message.author.name.lower()}-{str(ctx.message.author.id)[:8]}.')
     else:
         generate_cards(playerDeck.get(str(ctx.message.author.id)[:8]))
         await ctx.send('Here\'s your deck!', file=discord.File('last.png', 'deck.png'))
